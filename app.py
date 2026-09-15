71ccf3be3c53af8bcabd62e0b51244ff9306ffa4
Luisinho <luizinho1797@gmail.com>
2026-09-14 21:08:59 -0300

    Corrige conexão PostgreSQL com psycopg

--- app.py
+++ app.py
@@ -17,6 +17,12 @@ DATABASE_URL = os.getenv(
     f"sqlite:///{BASE_DIR}/alunos.db"
 )
 
+if DATABASE_URL.startswith("postgresql://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1
+    )
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(
         DATABASE_URL,
